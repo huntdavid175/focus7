7444ded949a203f83f7f5d48d3d420a220232ec6
Fawaz <fawaz@Fawazs-MacBook-Air.local>
2024-03-22 13:26:35 -0400

    closing file after writing

--- f7.c
+++ f7.c
@@ -4,17 +4,28 @@ int main(void)
 {
     FILE *fp = NULL;
 
+    FILE *fpb = NULL;
+
     if ((fp = fopen("text.txt", "w")) != NULL)
     {
-        if (fprintf(fp, "This is first line\n") < 0)
+
+        for (int i = 0; i < 2; i++)
         {
-            printf("Cant write to file");
-            return -2;
+            if (fprintf(fp, "This is line %d\n", i + 1) < 0)
+            {
+                printf("Cant write to file");
+                return -2;
+            }
         }
     }
     else
     {
         printf("File not opened");
+    }
+
+    if (fclose(fp) != 0)
+    {
+        print("Cannot close file");
     }
 
     return 0;
